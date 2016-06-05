3a712ea56083f40e291f170882b78d793a029e78
Matt Jenkins <matt@majenko.co.uk>
2016-06-05 15:23:05 +0100

    Initial Import

---
+++ pins_arduino.h
@@ -0,0 +1,12 @@
+#ifndef _PINS_ARDUINO_H
+#define _PINS_ARDUINO_H
+
+#include <wiring_pins.h>
+
+extern struct _pin _pins_gpio[];
+
+#define __SERIAL_0 "/dev/ttyAMA0"
+
+#define NUM_GPIO 40
+
+#endif
